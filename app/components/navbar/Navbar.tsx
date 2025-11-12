06a8b8aa583ff479b0b3cf8302a68845923e5896
Aliah Guinal <aliah.guinal@msunaawan.edu.ph>
2025-11-12 18:12:42 +0800

    wapa na homan part 2

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 import SearchFilters from './SearchFilters';
+import UserNav from './UserNav';
+import AddPropertyButton from './AddPropertyButton';
 
 const Navbar = () => {
     return (
@@ -22,7 +24,9 @@ const Navbar = () => {
                     </div>
 
                     <div className="flex items-center space-x-6">
-                        Add property - User nav
+                        <AddPropertyButton />
+
+                        <UserNav />
                     </div>
                 </div>
             </div>
